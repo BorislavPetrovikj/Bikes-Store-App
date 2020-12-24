04e97af13300d2d35aef9eef990f9d915611524e
Borko <petrovicborko392@gmail.com>
2020-12-24 12:46:25 +0100

    Added Changes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,38 +5,61 @@ import HeaderStyle from "./Header.css";
 class Card extends Component {
   state = {
     products: [],
+    productsToShow: [],
+    allBrands: {
+      KROSS: 0,
+      "LE GRAND BIKES": 0,
+      EXPLORER: 0,
+      VISITOR: 0,
+      FORCE: 0,
+      PONY: 0,
+      "E-BIKES": 0,
+      IDEAL: 0,
+    },
   };
 
   componentDidMount() {
-    axios(`https://json-project3.herokuapp.com/products`).then((res) => {
-      console.log(res.data);
-      this.setState({
-        products: res.data,
+    fetch("https://json-project3.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((el) => {
+          if (this.state.allBrands[el.brand] < 2) {
+            this.state.productsToShow.push(el);
+            this.state.allBrands[el.brand]++;
+          }
+        });
+        this.setState({
+          products: data,
+          productsToShow: this.state.productsToShow,
+        });
       });
-    });
   }
 
   render() {
     return (
-      <div class="row ">
-        {this.state.products.map((product, index) => (
-          <div key={index}>
-            <div class="col-md-4 card-cont">
-              <a href="#" class="inner">
-                <div class="img-cont">
-                  <img
-                    src={`/img/${product.image}.png`}
-                    class="card-img img img-responsive"
-                  ></img>
-                </div>
-                <div class="content-cont">
-                  <h2>{product.name}</h2>
-                  <p class="price">${product.price} $</p>
-                </div>
-              </a>
-            </div>
-          </div>
-        ))}
+      <div className="Card row">
+        {this.state.productsToShow.map((el) => {
+          return (
+            <>
+              <div className="col-md-4 card-cont">
+                <a href="#" class="inner">
+                  <div className="img-cont">
+                    <img
+                      className="img-cont"
+                      src={`/img/${el.image}.png`}
+                      className="card-img img img-responsive"
+                      alt="card"
+                    />
+                  </div>
+                  <div className="content-cont">
+                    <h6>{el.name}</h6>
+                    <p className="price">{el.price} $</p>
+                  </div>
+                </a>
+              </div>
+            </>
+          );
+        })}
       </div>
     );
   }
